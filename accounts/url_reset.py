a68a43807aa634884389b9f571fb419a536b8baa
MelBiggs <melissabiggs16@gmail.com>
2020-03-04 15:51:33 +0000

    Added app level urls for password reset

--- accounts/url_reset.py
+++ accounts/url_reset.py
@@ -4,7 +4,8 @@ from django.contrib.auth.views import password_reset, password_reset_done, passw
 
 
 urlpatterns = [
-    url('^$', password_reset, {'post_reset_redirect': reverse_lazy('password_reset_done')}, name='password_reset'),
+    url('^$', password_reset, {'post_reset_redirect': reverse_lazy
+        ('password_reset_done')}, name='password_reset'),
     url(r'^done/$', password_reset_done, name='password_reset_done'),
     url(r'^(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', password_reset_confirm,
         {'post_reset_redirect': reverse_lazy('password_reset_complete')}, name='password_reset_confirm'),
