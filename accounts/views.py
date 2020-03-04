4c6d179e38b4ae98981ac298d17ecc98cdd3e8a4
MelBiggs <melissabiggs16@gmail.com>
2020-03-04 11:31:14 +0000

    Added login page

--- accounts/views.py
+++ accounts/views.py
@@ -13,3 +13,7 @@ def logout(request):
     auth.logout(request)
     messages.success(request, "You have successfully been logged out!")
     return redirect(reverse('index'))
+
+def login(request):
+    """Return a login page"""
+    return render(request, 'login.html')
