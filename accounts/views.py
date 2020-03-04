07fec29cf6bb0fed972fdf09df3881d88ea48e81
MelBiggs <melissabiggs16@gmail.com>
2020-03-04 12:35:14 +0000

    Added authentication to login and base.html file

--- accounts/views.py
+++ accounts/views.py
@@ -17,5 +17,19 @@ def logout(request):
 
 def login(request):
     """Return a login page"""
-    login_form = UserLoginForm()
+    if request.method == 'POST':
+        login_form = UserLoginForm(request.POST)
+
+        if login_form.is_valid():
+            user = auth.authenticate(username=request.POST['username'],
+                                     password=request.POST['password'])
+
+            if user:
+                auth.login(user=user, request=request)
+                messages.success(request, "You have successfully logged in!")
+
+            else:
+                login_form.add_error(None, "Your username or password is incorrect")
+    else:
+        login_form = UserLoginForm
     return render(request, 'login.html', {"login_form": login_form})
