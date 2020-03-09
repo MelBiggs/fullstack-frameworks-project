a7be75c057aef937e1d78bd2ccdb3aab60923f3e
MelBiggs <melissabiggs16@gmail.com>
2020-03-09 12:33:33 +0000

    Worked on product views and urls

--- facevalue/urls.py
+++ facevalue/urls.py
@@ -17,9 +17,14 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from accounts.views import index
 from accounts import urls as accounts_urls
+from products import urls as urls_products
+from django.views import static
+from .settings import MEDIA_ROOT
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', index, name="index"),
-    url(r'^accounts/', include(accounts_urls))
+    url(r'^$', index, name='index'),
+    url(r'^accounts/', include(accounts_urls)),
+    url(r'^products/', include(urls_products)),
+    url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT})
 ]
