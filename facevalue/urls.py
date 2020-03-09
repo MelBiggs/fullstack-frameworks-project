30ebf211f55fecaa56bf683ec7e2c23972a7c419
MelBiggs <melissabiggs16@gmail.com>
2020-03-09 12:57:27 +0000

    Shopping cart items stored in session

--- facevalue/urls.py
+++ facevalue/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from accounts.views import index
 from accounts import urls as accounts_urls
 from products import urls as urls_products
+from cart import urls as urls_cart
 from django.views import static
 from .settings import MEDIA_ROOT
 
@@ -26,5 +27,6 @@ urlpatterns = [
     url(r'^$', index, name='index'),
     url(r'^accounts/', include(accounts_urls)),
     url(r'^products/', include(urls_products)),
+    url(r'^cart/', include(urls_cart)),
     url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT})
 ]
