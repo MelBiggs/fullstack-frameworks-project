f6a29d9327d98ae59556d4e1be8c918c48910962
MelBiggs <melissabiggs16@gmail.com>
2020-03-09 15:34:03 +0000

    Added search capabilities and updated base.html with urls

--- facevalue/urls.py
+++ facevalue/urls.py
@@ -19,6 +19,7 @@ from accounts.views import index
 from accounts import urls as accounts_urls
 from products import urls as urls_products
 from cart import urls as urls_cart
+from search import urls as urls_search
 from django.views import static
 from .settings import MEDIA_ROOT
 
@@ -28,5 +29,6 @@ urlpatterns = [
     url(r'^accounts/', include(accounts_urls)),
     url(r'^products/', include(urls_products)),
     url(r'^cart/', include(urls_cart)),
+    url(r'^search/', include(urls_search)),
     url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT})
 ]
