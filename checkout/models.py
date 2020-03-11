e2ae6212d518a98391c0ed82e02a27efb07d74c7
MelBiggs <melissabiggs16@gmail.com>
2020-03-11 11:58:06 +0000

    Added checkout form, updated checkout model and updated django version

--- checkout/models.py
+++ checkout/models.py
@@ -7,7 +7,6 @@ class Order(models.Model):
     full_name = models.CharField(max_length=50, blank=False)
     phone_number = models.CharField(max_length=20, blank=False)
     country = models.CharField(max_length=40, blank=False)
-    postcode = models.CharField(max_length=20, blank=True)
     town_or_city = models.CharField(max_length=40, blank=False)
     street_address1 = models.CharField(max_length=40, blank=False)
     street_address2 = models.CharField(max_length=40, blank=False)
