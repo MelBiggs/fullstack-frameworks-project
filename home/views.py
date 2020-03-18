374f614e64e463910bf418528442040a8eb2493b
MelBiggs <melissabiggs16@gmail.com>
2020-03-18 10:17:34 +0000

    Got product cards displaying on index

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from products.models import Product
 
 def index(request):
     """A view that displays the index page"""
-    return render(request, "index.html")
+    products = Product.objects.all()
+    return render(request, "index.html", {"products": products})
