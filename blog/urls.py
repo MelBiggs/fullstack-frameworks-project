e9af648ed2878ffa0ac27b6ad3dfb4d866d73dab
MelBiggs <melissabiggs16@gmail.com>
2020-03-24 11:15:11 +0000

    Worked on CSS of blog post form

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
-from .views import get_posts, post_detail, create_or_edit_post, new_comment
+from .views import get_posts, post_detail, create_or_edit_post
+# new_comment
 
 urlpatterns = [
     url(r'^$', get_posts, name='get_posts'),
     url(r'^(?P<pk>\d+)/$', post_detail, name='post_detail'),
     url(r'^new/$', create_or_edit_post, name='new_post'),
     url(r'^(?P<pk>\d+)/edit/$', create_or_edit_post, name='edit_post'),
-    url(r'^(?P<pk>\d+)/comment/$/', new_comment, name='new_comment')
+    # url(r'^(?P<pk>\d+)/comment/$/', new_comment, name='new_comment')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-from .models import Post, Comment
+from .models import Post
 from .forms import BlogPostForm
 
 
@@ -49,23 +49,23 @@ def create_or_edit_post(request, pk=None):
 # Comments
 
 
-def new_comment(request, pk):
-    if not request.user.is_authenticated:
-        return redirect('login')
-    else:
-        try:
-            if (request.user.is_superuser is not None):
-                comments = Comment.objects.all()
-                post = get_object_or_404(Post, pk=pk)
-                blogs = Post.objects.all()
-                if request.method == "POST":
-                    form = CommentForm(request.POST)
-                    if form.is_valid():
-                        comment = form.save(commit=False)
-                        comment.owner = request.user
-                        comment.post = post
-                        comment.save()
-                        return redirect('post_detail', pk=post.pk)
-                else:
-                    form = CommentForm()
-    return render(request, 'posts/commentform.html', {'form': form, 'comments': comments, 'post': post})
+# def new_comment(request, pk):
+#     if not request.user.is_authenticated:
+#         return redirect('login')
+#     else:
+#         try:
+#             if (request.user.is_superuser is not None):
+#                 comments = Comment.objects.all()
+#                 post = get_object_or_404(Post, pk=pk)
+#                 blogs = Post.objects.all()
+#                 if request.method == "POST":
+#                     form = CommentForm(request.POST)
+#                     if form.is_valid():
+#                         comment = form.save(commit=False)
+#                         comment.owner = request.user
+#                         comment.post = post
+#                         comment.save()
+#                         return redirect('post_detail', pk=post.pk)
+#                 else:
+#                     form = CommentForm()
+#     return render(request, 'posts/commentform.html', {'form': form, 'comments': comments, 'post': post})
