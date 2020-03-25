362ebda682fb883db22c101fb93f51825e056579
MelBiggs <melissabiggs16@gmail.com>
2020-03-25 15:59:26 +0000

    Added searching capability for blog posts

--- search/urls.py
+++ search/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import do_search
+from .views import do_search, do_blog_search
 
 urlpatterns = [
-    url(r'^$', do_search, name='search')
+    url(r'^$', do_search, name='search'),
+    url(r'^blogs', do_blog_search, name='blogsearch')
 ]
--- search/views.py
+++ search/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from products.models import Product
+from blog.models import Post
 
 
 def do_search(request):
     products = Product.objects.filter(name__icontains=request.GET['q'])
     return render(request, "products.html", {"products": products})
+
+
+def do_blog_search(request):
+    posts = Post.objects.filter(title__icontains=request.GET['q'])
+    return render(request, "blogposts.html", {"posts": posts})
