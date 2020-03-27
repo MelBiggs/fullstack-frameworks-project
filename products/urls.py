ee9a487d48e811b1db63844086962ee68956396d
MelBiggs <melissabiggs16@gmail.com>
2020-03-27 15:22:44 +0000

    Added productdetails.html page

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from .views import all_products, face_products, body_products
+from .views import all_products, face_products, body_products, product_detail
 
 urlpatterns = [
     url(r'^$', all_products, name='products'),
+    url(r'^(?P<pk>\d+)/$', product_detail, name='product_detail'),
     url(r'^face', face_products, name='face_products'),
     url(r'^body', body_products, name='body_products'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
@@ -15,6 +15,23 @@ def face_products(request):
 def body_products(request):
     products = Product.objects.all().filter(category='B')
     return render(request, "products.html", {"products": products})
+
+
+def product_detail(request, pk):
+    """
+    Create a view that returns a single
+    Product object based on the product ID (pk) and
+    render it to the 'productdetail.html' template.
+    Or return a 404 error if the post is
+    not found
+    """
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, "productdetail.html", {'product': product})
+
+
+# @login_required
+# def like_product(request):
+
 
 
 # def filter_tags(request):
