ad8a1cdb0fa8b29785adf79d261db7c47e73e892
MelBiggs <melissabiggs16@gmail.com>
2020-03-28 17:24:39 +0000

    Added paginator to blog view and html

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import BlogPostForm
+from django.core.paginator import Paginator
+
+ITEMS_PER_PAGE = 3
 
 
 def get_posts(request):
@@ -12,6 +15,17 @@ def get_posts(request):
     """
     posts = Post.objects.filter(published_date__lte=timezone.now()
                                 ).order_by('-published_date')
+
+    paginator = Paginator(posts, ITEMS_PER_PAGE)
+
+    page = request.GET.get('page')
+
+    # If there is no page parameter in URL, set page to 1
+
+    if(page is None):
+        page = 1
+
+    posts = paginator.page(page)
     return render(request, "blogposts.html", {'posts': posts})
 
 
