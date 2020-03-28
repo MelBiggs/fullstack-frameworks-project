3f746025d18d8f0208c92bf68ad829ec10eca234
MelBiggs <melissabiggs16@gmail.com>
2020-03-28 17:24:22 +0000

    Added paginator to all product views

--- products/views.py
+++ products/views.py
@@ -25,11 +25,32 @@ def all_products(request):
 
 def face_products(request):
     products = Product.objects.all().filter(category='F')
+
+    paginator = Paginator(products, ITEMS_PER_PAGE)
+
+    page = request.GET.get('page')
+
+    # If there is no page parameter in URL, set page to 1
+
+    if(page is None):
+        page = 1
+
+    products = paginator.page(page)
     return render(request, "products.html", {"products": products})
 
 
 def body_products(request):
     products = Product.objects.all().filter(category='B')
+    paginator = Paginator(products, ITEMS_PER_PAGE)
+
+    page = request.GET.get('page')
+
+    # If there is no page parameter in URL, set page to 1
+
+    if(page is None):
+        page = 1
+
+    products = paginator.page(page)
     return render(request, "products.html", {"products": products})
 
 
