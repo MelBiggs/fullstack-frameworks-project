8b2771fc6b2842c0514347df786472b1beadffdb
MelBiggs <melissabiggs16@gmail.com>
2020-04-02 21:42:20 +0000

    Small changes to check if Travis responds

--- facevalue/settings.py
+++ facevalue/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'fullstackframeworks-project.herokuapp.com']
+# ALLOWED_HOSTS = ['fullstackframeworks-project.herokuapp.com']
 
 
 # Application definition
