c92b705921f791eb1fa15ba5844df13892f3b9b9
MelBiggs <melissabiggs16@gmail.com>
2020-04-03 14:25:46 +0000

    Removed env try statement from settings to test in Travis

--- facevalue/settings.py
+++ facevalue/settings.py
@@ -12,11 +12,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import dj_database_url
+import env
 
-try:
-    import env
-except ModuleNotFoundError:
-    pass
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
