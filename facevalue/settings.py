d777ec3a7b0b423526cf5e3c5c667b87295ce8ca
MelBiggs <melissabiggs16@gmail.com>
2020-04-03 14:29:22 +0000

    Changed env in settings for Travis

--- facevalue/settings.py
+++ facevalue/settings.py
@@ -12,8 +12,10 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import dj_database_url
-import env
 
+from os import path
+if path.exists("env.py"):
+   import env
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
