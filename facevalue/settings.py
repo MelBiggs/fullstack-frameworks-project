0874431d4d0e2074cbf44b6aa7fe581acfd993f9
MelBiggs <melissabiggs16@gmail.com>
2020-04-22 13:06:22 +0000

    Finishing touches

--- facevalue/settings.py
+++ facevalue/settings.py
@@ -175,8 +175,8 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+EMAIL_HOST_USER = os.getenv("EMAIL_ADDRESS")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_PASSWORD")
 EMAIL_PORT = 587
 
 
